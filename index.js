f32b77cac9ba9c956254ca36f1ab1106b4069af0
Codeyg12 <codey.gallup@gmail.com>
2023-04-15 12:11:47 -0600

    using key and testing

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const firstButton = document.getElementById('first-search-button')
 const secondButton = document.getElementById('second-search-button')
 
 
-const searchBikes = async () => {
-    const res = await fetch('https://api.api-ninjas.com/v1/motorcycles?make=kawasaki&model=ninja')
+const searchBikes = async (e) => {
+    console.log(e)
+    e.preventDefault()
+    const res = await fetch('https://api.api-ninjas.com/v1/motorcycles?make=kawasaki&model=ninja', {
+        headers: {
+            'X-Api-Key': '7DnNLomBLGckBQ2OXbFq3w==WFA9KJrbx9bM7f5G'
+        }
+    })
     const answer = await res.json()
     console.log(answer)
 }
