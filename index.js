54f597ee33217584d8c538d5be26264a74fc7990
Codeyg12 <codey.gallup@gmail.com>
2023-04-18 18:51:16 -0600

    changed function from being seperate into arrow function

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const firstButton = document.getElementById('first-search-button')
 const secondButton = document.getElementById('second-search-button')
 const firstSearchValue = document.getElementById('first-search')
 
+//!MAYBEMAYBEMAYBE
+// const searchBikes = async (e) => {
+//     console.log(e)
+//     const fValue = firstSearchValue.value
+//     console.log(firstSearchValue.value)
+//     e.preventDefault()
+//     const res = await fetch(`https://api.api-ninjas.com/v1/motorcycles?model=${fValue}`, {
+//         headers: {
+//             'X-Api-Key': '7DnNLomBLGckBQ2OXbFq3w==WFA9KJrbx9bM7f5G'
+//         }
+//     })
+//     const answer = await res.json()
+//     console.log(answer)
+// }
 
-const searchBikes = async (e) => {
-    console.log(e)
+// firstButton.addEventListener('click', searchBikes)
+secondButton.addEventListener('click', async (e) => {
+        console.log(e)
     const fValue = firstSearchValue.value
     console.log(firstSearchValue.value)
     e.preventDefault()
@@ -15,7 +30,4 @@ const searchBikes = async (e) => {
     })
     const answer = await res.json()
     console.log(answer)
-}
-
-firstButton.addEventListener('click', searchBikes)
-secondButton.addEventListener('click', searchBikes)
+})
